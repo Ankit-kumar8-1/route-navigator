486d659a8e52b7bc3c912172c249535725a484f5
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-03-12 11:33:37 +0530

    Initial project setup with Spring Boot 4, Java 17, added dependencies (web, jpa, mysql, lombok, validation), configured MySQL database connection and OpenRouteService API key in application.properties

---
+++ src/main/java/com/navigation/route_navigator/RouteNavigatorApplication.java
@@ -0,0 +1,14 @@
+package com.navigation.route_navigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteNavigatorApplication.class, args);
+        System.out.println("Application is running.....");
+	}
+
+}
---
+++ src/test/java/com/navigation/route_navigator/RouteNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navigation.route_navigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
